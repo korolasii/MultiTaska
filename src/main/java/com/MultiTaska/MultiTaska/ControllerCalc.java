dd083db126bdd93d4095baf1c758e0f60c277556
korolasii <timofei.kropachev@gmail.com>
2024-02-03 22:16:45 +0200

    add calculate

--- src/main/java/com/MultiTaska/MultiTaska/ControllerCalc.java
+++ src/main/java/com/MultiTaska/MultiTaska/ControllerCalc.java
@@ -2,6 +2,8 @@ package com.MultiTaska.MultiTaska;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ControllerCalc {
@@ -9,5 +11,11 @@ public class ControllerCalc {
     @GetMapping("/calc")
     public String calc() {
         return "calc";
+    }
+
+    @PostMapping("/calculate")
+    public String submit(@RequestBody Calculate inputRequest) {
+        String inputValue = inputRequest.getInputValue();
+        return "{\"returnedValue\": \"" + inputValue + "\"}";
     }
 }
