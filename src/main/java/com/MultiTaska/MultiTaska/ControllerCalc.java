b655f28515fee989bf99bde1ed3a28f5c9d03f6c
korolasii <timofei.kropachev@gmail.com>
2024-02-07 19:16:26 +0200

    add calculate

--- src/main/java/com/MultiTaska/MultiTaska/ControllerCalc.java
+++ src/main/java/com/MultiTaska/MultiTaska/ControllerCalc.java
@@ -1,5 +1,9 @@
 package com.MultiTaska.MultiTaska;
 
+import ch.qos.logback.core.boolex.JaninoEventEvaluatorBase;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerCalc {
 
+
     @PostMapping("/calculate")
-    public int calculate(@RequestBody String expression) {
-        return 42;
+    public double calculate(@RequestBody String expression) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(expression);
+        String expressionCalc = jsonNode.get("expression").asText();
+        return Calculate.evaluateExpression(expressionCalc);
     }
 }
 
